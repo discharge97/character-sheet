a6afdbe140e1990aaa45eb90a5a367a1b2fb8b8b
Aleksandar Stojadinovic <aleksandar.stojadinovic@netsetglobal.rs>
2025-06-22 20:17:51 +0200

    Added release script for prep

---
+++ scripts/release.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+rm -rf release
+mkdir release
+
+version=v$(cat package.json | grep "version" | cut -d'"' -f4 )
+cp ./android/app/build/outputs/apk/debug/app-debug.apk ./release/dnd_character_sheet_"$version".apk &&
+bestzip ./release/angular_build_"$version".7z ./dist/character-sheet/browser/*
+
+echo "
+Version: $version"
