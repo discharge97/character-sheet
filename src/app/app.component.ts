9ea0ac53c14e877e4627401539736200b899ece7
Aleksandar Stojadinovic <aleksandar.stojadinovic@netsetglobal.rs>
2025-08-03 21:58:09 +0200

    Handeled mobile back button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,10 @@ import {Component, OnDestroy} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {App} from "@capacitor/app";
 import {PluginListenerHandle} from "@capacitor/core";
+import {CharacterService} from "./services/character.service";
+import {MatDialog} from "@angular/material/dialog";
+import {ConfirmationDialogComponent} from "./dialogs/confirmation-dialog/confirmation-dialog.component";
+import {firstValueFrom} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -13,10 +17,20 @@ export class AppComponent implements OnDestroy {
   title = 'character-sheet';
   private listener?: PluginListenerHandle;
 
-  constructor() {
+  constructor(private charService: CharacterService, private dialog: MatDialog) {
     App.addListener('backButton', ({canGoBack}) => {
       if (canGoBack) {
-        window.history.back();
+        if (this.charService.hasChanges.value) {
+          firstValueFrom(this.dialog.open(ConfirmationDialogComponent,
+            {
+              data: {
+                title: "Are you sure you can to exit character sheet? You have unsaved changes!"
+              }
+            }).afterClosed()).then((res?: boolean) => {
+            if (!res) return;
+            window.history.back();
+          });
+        }
       } else {
         App.exitApp();
       }
